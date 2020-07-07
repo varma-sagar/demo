959dc4217b8c76dccf9472bef719f36753f6d039
varma-sagar <sagar.varma@salesforce.com>
2020-07-07 20:03:22 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public String index(){
+		return "Hello Spring Boot";
+	}
+
+	@GetMapping("/hello/{id}")
+	public String Error(@PathVariable String id){
+		return "Error Page"+id;
+	}
+
+}
